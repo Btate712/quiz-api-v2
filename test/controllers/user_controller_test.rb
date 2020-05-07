5e5b8cd3b8840847f0cbe1ae15acdd4e98433290
btate712 <btate712@gmail.com>
2020-05-07 18:52:19 -0500

    added test for post /users route

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -40,4 +40,10 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     user = JSON.parse(@response.body)["users"].first
     assert_not user.has_key?("password_digest")
   end
+
+  test "post /users request creates a new user when supplied with the appropriate data" do
+    beforeUserCount = User.all.count
+    post users_url(name: "NewUser01", password: "password", email: "someEmail@gmail.com")
+    assert User.all.count == beforeUserCount + 1 
+  end
 end
