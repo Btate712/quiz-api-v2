aa587130627ba263ea9340c2b095a51280e95ce0
btate712 <btate712@gmail.com>
2020-05-06 20:35:08 -0500

    added is_admin field to json rendered for users index route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
      render json: {
        message: "Users...",
-       users: User.all.as_json(only: [:name])
+       users: User.all.as_json(only: [:name, :is_admin])
      }
   end
 end
