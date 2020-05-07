204a5effedf163170c27488a29d803c6b4e849cd
btate712 <btate712@gmail.com>
2020-05-06 19:37:38 -0500

    added validation and test to verify project names cannot be duplicates

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,6 @@ class Project < ApplicationRecord
   has_many :topics
 
   validates_presence_of :name
+
+  validates_uniqueness_of :name
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -3,11 +3,16 @@ require 'test_helper'
 class ProjectTest < ActiveSupport::TestCase
   test "saves a project with a name" do
     project = Project.new(name: "My Project")
-    assert project.save
+    assert project.save, "Project failed to save"
   end
   
   test "does not save a project without a name" do
     project = Project.new()
-    assert_not project.save
+    assert_not project.save, "Project saved without a name"
+  end
+
+  test "does not save a project with the same name as an existing project" do
+    project = Project.new(name: Project.first.name);
+    assert_not project.save, "Project saved with same name as existing project"
   end
 end
