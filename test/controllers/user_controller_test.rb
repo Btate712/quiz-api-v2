84370d97780295a1585115a7569a57aa8402b5a0
btate712 <btate712@gmail.com>
2020-05-07 16:34:56 -0500

    wrote tests for get /users route

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @headers = {
+      "Content-type": "application/json"
+    }
+  end
+
+  test "get /users request is successful" do
+    get users_url
+    assert_response :success
+  end
+
+  test "get /users request returns text that includes the word 'users'" do
+    get users_url
+    assert @response.body.include?("users")
+  end
+
+  test "get /users request returns JSON that includes an array of all users" do
+    get users_url
+    users = JSON.parse(@response.body)["users"]
+    assert users.count === User.all.count
+  end
 end
