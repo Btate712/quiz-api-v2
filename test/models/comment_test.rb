fdd14a332f992b90a7cf1194da1b25e408040575
btate712 <btate712@gmail.com>
2020-05-06 20:14:01 -0500

    refactored comment tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,75 +2,39 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   test "saves a comment with a user_id, question_id, comment_type, and content" do
-    project = Project.create(name: "My Project")
-    topic = Topic.create(name: "something", project_id: project.id)
-    user = User.create(name: "bob", email: "bob@bob.bob", password: "temp")
-    question = Question.create(
-      stem: "This is the question",
-      choice_1: "A",
-      choice_2: "B",
-      choice_3: "C",
-      choice_4: "D",
-      correct_choice: 2,
-      topic_id: topic.id
-    )
     comment = Comment.new(
-      user_id: user.id, 
-      question_id: question.id, 
+      user_id: users(:one).id, 
+      question_id: questions(:one).id, 
       comment_type: "test",
       content: "Some comment..."
     )
-    assert comment.save
+    assert comment.save, "failed to save comment with a user_id, question_id, comment_type, and content"
   end
   
   test "does not save a comment without a user_id" do
-    project = Project.create(name: "My Project")
-    topic = Topic.create(name: "My Topic", project_id: project.id)
-    question = Question.create(
-      stem: "This is the question",
-      choice_1: "A",
-      choice_2: "B",
-      choice_3: "C",
-      choice_4: "D",
-      correct_choice: 2,
-      topic_id: topic.id
-    )
     comment = Comment.new(
-      question_id: question.id, 
+      question_id: questions(:one).id, 
       comment_type: "test",
       content: "Some comment..."
     )
-    assert_not comment.save
+    assert_not comment.save, "saved comment without a user_id"
   end
   
   test "does not save a comment without a question_id" do
-    user = User.create(name: "bob", email: "bob@bob.bob", password: "temp")
     comment = Comment.new(
-      user_id: user.id, 
+      user_id: users(:one).id, 
       comment_type: "test",
       content: "Some comment..."
     )
-    assert_not comment.save
+    assert_not comment.save, "saved comment without a question_id"
   end
 
   test "does not save a comment without content" do
-    project = Project.create(name: "My Project")
-    topic = Topic.create(name: "My Topic", project_id: project.id)
-    user = User.create(name: "bob", email: "bob@bob.bob", password: "temp")
-    question = Question.create(
-      stem: "This is the question",
-      choice_1: "A",
-      choice_2: "B",
-      choice_3: "C",
-      choice_4: "D",
-      correct_choice: 2,
-      topic_id: topic.id
-    )
     comment = Comment.new(
-      user_id: user.id,
+      user_id: users(:one).id,
       comment_type: "test",
-      question_id: question.id
+      question_id: questions(:one).id
     )
-    assert_not comment.save
+    assert_not comment.save, "saved comment without content"
   end
 end
