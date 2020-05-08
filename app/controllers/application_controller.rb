a99494570463816b0c8d073015df2c7a4e6cb724
btate712 <btate712@gmail.com>
2020-05-07 20:05:28 -0500

    built User show and delete routes and controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  attr_reader :current_user
 
+  #TODO: fix CSRF issues
+  skip_before_action :verify_authenticity_token
+  
+  private
+  #TODO: set current user via JWT
+  def current_user
+    User.find_by(name: "admin")
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     if new_user.save
       response = { 
         message: "New user '#{new_user.name}' created successfully",
+        user: new_user.as_json(except: [:password_digest]),
         status: :success
       }
     else
@@ -21,6 +22,48 @@ class UsersController < ApplicationController
         message: "New user failed to save.",
         status: :failure,
         errors: new_user.errors
+      }
+    end
+    render json: response
+  end
+
+  def show
+    if !current_user.is_admin
+      response = {
+        message: "Viewing User data requires admin access",
+        status: :failure
+      }
+    elsif !User.find_by(id: params[:id])
+      response = {
+        message: "User with id: '#{params[:id]}' not found",
+        status: :failure
+      }
+    else
+      response = { 
+        message: "User found.",
+        user: User.find_by(id: params[:id]).as_json(except: [:password_digest]),
+        status: :success
+      }
+    end
+    render json: response
+  end
+
+  def destroy
+    if !current_user.is_admin
+      response = {
+        message: "Deleting User data requires admin access",
+        status: :failure
+      }
+    elsif !User.find_by(id: params[:id])
+      response = {
+        message: "User with id: '#{params[:id]}' not found",
+        status: :failure
+      }
+    else
+      User.find_by(id: params[:id]).destroy
+      response = { 
+        message: "User with id: '#{params[:id]}' deleted",
+        status: :success
       }
     end
     render json: response
