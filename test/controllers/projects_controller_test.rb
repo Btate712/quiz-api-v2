c7c35c4fab2ab8b92f3a70f08e4404313aa8e3e7
btate712 <btate712@gmail.com>
2020-05-08 03:50:24 -0500

    added get /topics[:id] route and tests

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -8,22 +8,37 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "get /projects request is successful" do
-    get projects_url
+    get projects_url, headers: @headers
     assert_response :success
   end
 
   test "get /projects request response includes an array of projects" do 
-    get projects_url 
+    get projects_url, headers: @headers 
     assert !!JSON.parse(@response.body)["projects"]
   end
 
   test "get /projects request response includes project names" do 
-    get projects_url 
+    get projects_url, headers: @headers 
     assert !!JSON.parse(@response.body)["projects"].first.has_key?("name")
   end
 
   test "get /projects request response includes project is_public fields" do 
-    get projects_url 
+    get projects_url, headers: @headers 
     assert !!JSON.parse(@response.body)["projects"].first.has_key?("is_public")
+  end
+
+  test "get /projects[:id] request is successful" do 
+    get project_url(1), headers: @headers
+    assert_response :success
+  end
+
+  test "get /projcects[:id] request response includes the project name" do 
+    get project_url(1), headers: @headers 
+    assert JSON.parse(@response.body)["project"].has_key?("name")
+  end
+
+  test "get /projcects[:id] request response includes the project is_public flag" do 
+    get project_url(1), headers: @headers 
+    assert JSON.parse(@response.body)["project"].has_key?("is_public")
   end
 end
