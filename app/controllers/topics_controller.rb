2bb478d300f2e7661959b84fd3ce129b460bac8d
btate712 <btate712@gmail.com>
2020-05-10 03:55:00 -0500

    built topics_controller show method and tests

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -9,6 +9,7 @@ class TopicsController < ApplicationController
         user_project ? project.user_projects.find_by(user_id: current_user.id).access_level : ADMIN_LEVEL,
       is_public: project.is_public,
       topics: project.topics.map{ |topic| {
+        topic_id: topic.id,
         topic_name: topic.name,
         number_of_questions: topic.questions.count
       }
@@ -19,7 +20,31 @@ class TopicsController < ApplicationController
   end
   
   def show
-
+    topic = Topic.all.find(params[:id])
+    user_project = topic.project.user_projects.find_by(user_id: current_user.id)
+    if (!topic)
+      response = {
+        message: "Topic with id: #{params[:id]} not found.",
+        status: :failure
+      }
+    elsif(!current_user.has_project_rights?(topic.project))
+      response = {
+        message: "You do not have read access to topic with id: #{params[:id]}.",
+        status: :failure
+      }
+    else
+      response = {
+        message: "Topic found.",
+        topic: {
+          id: topic.id,
+          name: topic.name,
+          access_level: user_project ? user_project.access_level : ADMIN_LEVEL,
+          questions: topic.questions
+        },
+        status: :success
+      }
+    end
+    render json: response
   end
 
   def create
--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -2,16 +2,25 @@ require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
   # index
-  test "/topics response includes a list of topics" do
-    get topics_url
-    @response.include?("topics")
+  test "get /topics response includes a list of topics" do
+    get topics_url, headers: @headers
+    assert @response.body.include?("topics")
   end
 
   # TODO: ensure that only topics that the user has access to are shown
-  
+
   # show
+  test "get /topics/[:id] response includes a topic along with its questions" do 
+    get topic_url(1), headers: @headers
+    assert @response.body.include?("questions")
+  end
 
   # create
+  test "post /topics creates a new topic" do
+    topic_count_before_create = Topic.count
+    post topics_url(name: "some new topic", project_id: 1), headers: @headers
+    assert Topic.count = topic_count_before_create + 1
+  end
 
   # update
 
