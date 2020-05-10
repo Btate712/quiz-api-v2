85f56cb93ebca56c713f25611a9e1a8c13c5f0e5
btate712 <btate712@gmail.com>
2020-05-10 00:19:16 -0500

    stubbed out user_projects controller and controller tests

---
+++ app/controllers/user_projects_controller.rb
@@ -0,0 +1,21 @@
+class UserProjectsController < ApplicationController
+  def index
+
+  end
+
+  def show 
+
+  end 
+
+  def create 
+
+  end
+
+  def update 
+
+  end
+
+  def delete 
+
+  end
+end
---
+++ test/controllers/user_projects_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UserProjectsControllerTest < ActionDispatch::IntegrationTest
+  # index
+
+  # show
+
+  # create
+
+  # update
+
+  # delete
+end
