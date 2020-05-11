6716d7034ec2600e5a41e2d6c5be4e1a3c48acc1
btate712 <btate712@gmail.com>
2020-05-11 05:38:40 -0500

    stuck with rails / ruby version issue

---
+++ test/controllers/comments_controller_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class CommentsControllerTest < ActionDispatch::IntegrationTest  
+    # show
+    test "get /comments/[:id] response includes a comment along with its questions" do 
+      get comment_url(1), headers: @headers
+      assert @response.body.include?("questions")
+    end
+  
+    # create
+    test "post /comments creates a new comment" do
+      comment_count_before_create = comment.count
+      post comments_url(name: "some new comment", project_id: 1), headers: @headers
+      assert comment.count == comment_count_before_create + 1
+    end
+  
+    # update
+    test "comment can be updated via an update request to /comments/[:id]" do
+      new_content = "Some other comment content"
+      put comment_url(1, content: new_content), headers: @headers 
+      assert comment.find(1).content == new_content
+    end
+  
+    # delete
+    test "delete /comments/[:id] deletes a comment" do 
+      comment_count_before_delete = comment.count
+      delete comment_url(1), headers: @headers
+      assert comment.count == comment_count_before_delete - 1
+    end
+end
