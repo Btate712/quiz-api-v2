94bf416f1ec04650c83e17a587fae79166ed4b2e
btate712 <btate712@gmail.com>
2020-05-17 04:38:41 -0500

    jwt authentication adjustments

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,12 +2,14 @@ class AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
+    puts "attempting to login"
     @user = User.find_by(name: user_login_params[:username])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { 
+        status: :success,
         user: @user.as_json(except: [:password_digest, :created_at, :updated_at]), 
         jwt: token 
       }, status: :accepted
